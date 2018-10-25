0cf274cb69c01314046b836dbb22286ecf2c7b18
Joe Ardent <code@ardent.nebcorp.com>
2018-10-24 23:36:22 -0700

    Read in entire file, then process.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,60 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-fn get_vals() -> Vec<f64> {
-    let args = App::new("Cube Rooter")
+const VERSION: &str = "1";
+
+fn get_args() -> ArgMatches<'static> {
+    App::new("Cube Rooter")
+        .version(VERSION)
         .arg(
-            Arg::with_name("VALS")
-                .help("File to read that contains values to cube-root.")
+            Arg::with_name("NUMS_FILE")
+                .help("File to read that contains numbers to cube-root.")
                 .required(true)
-                .index(1),
+                .short("f")
+                .long("nums-file")
+                .last(false)
+                .takes_value(true),
         )
-        .get_matches();
+        .arg(
+            Arg::with_name("variant")
+                .help("Type of cube root function to use.")
+                .index(1)
+                .required(true)
+                .possible_values(&["exact", "approx", "simd"]),
+        )
+        .get_matches()
+}
 
-    let vals_name: String = args.value_of("VALS").unwrap().into();
+fn get_vals(args: &ArgMatches) -> Vec<f64> {
+    let vals_name: &str = args.value_of("NUMS_FILE").unwrap();
 
-    let path = Path::new(&vals_name);
+    let path = Path::new(vals_name);
 
-    let f = match File::open(&path) {
-        Ok(f) => f,
-        Err(e) => panic!(format!(
-            "could not open {:?} for reading, got {:?}",
-            &path, e
-        )),
-    };
+    let f = File::open(&path).unwrap_or_else(|_| panic!("Could not open {:#?}", &path));
 
-    let ref mut buf_reader = BufReader::new(&f);
+    let buf_reader = &mut BufReader::new(&f);
     let vals: Vec<f64> = buf_reader
         .lines()
-        .map(|x| x.unwrap_or("0.0".to_string()).parse::<f64>().unwrap())
-        .collect();
+        .map(|x| x.unwrap().parse::<f64>())
+        .filter_map(Result::ok).collect();
     vals
 }
 
+fn get_rooter(args: &ArgMatches) -> Box<dyn Fn(f64) -> f64> {
+    match args.value_of("variant").unwrap() {
+        _ => Box::new(move |x| x.cbrt()),
+    }
+}
+
 fn main() {
-    let vals = get_vals();
-    println!("reading from {:?}", vals)
+    let args = get_args();
+    let vals = get_vals(&args);
+    let rooter = get_rooter(&args);
+
+    for v in vals.into_iter() {
+        println!("{} {}", v, rooter(v));
+    }
 }
