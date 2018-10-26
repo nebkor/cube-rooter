5b7a79b7d1a2bb192c91835bf1727b1f3d32a8e8
Joe Ardent <code@ardent.nebcorp.com>
2018-10-25 22:40:38 -0700

    Adds 'approx' and 'fast-approx' cube root methods.
    'approx' means, 'treat float as int, divide by 3.0, add magic constant'.
    'fast-approx' means that, but approximate floating division with shifts and sums of infinite series,
    using strictly integer ops. It's *slightly* faster than approx, and almost
    exactly as accurate.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use clap::{App, Arg, ArgMatches};
 
+use std::f64;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 const VERSION: &str = "1";
+
+// see http://h14s.p5r.org/2012/09/0x5f3759df.html
+// The integer representation of a float transformed
+// by a fractional exponential function is:
+//
+// Iy = (1 - p) * L * (B - o) + p * Ix
+//
+// p = exPonent = 0.3333333 for cube root
+// L = 2^(mantissa bits) = 2^52 for double precision IEEE-754
+// B = "bias" in the biased exponent; see https://en.wikipedia.org/wiki/Double-precision_floating-point_format#IEEE_754_double-precision_binary_floating-point_format:_binary64
+// o = offset, for shifting the curve up or down
+// Ix = integer representation of IEEE 754 float
+
+// MAGIC = (1 - p) * L * (B - o)
+// 64-bit: ((2.0 * 4503599627370496.0f64 * (1023.0 - 0.035)) as u64) / 3;
+const MAGIC64: u64 = 0x2A9FA06D3A06D3A0;
+
+const ONE_THIRD: f64 = 1.0f64 / 3.0f64;
 
 fn get_args() -> ArgMatches<'static> {
     App::new("Cube Rooter")
@@ -24,7 +43,7 @@ fn get_args() -> ArgMatches<'static> {
                 .help("Type of cube root function to use.")
                 .index(1)
                 .required(true)
-                .possible_values(&["exact", "approx", "simd"]),
+                .possible_values(&["exact", "approx", "fast-approx", "simd"]),
         )
         .get_matches()
 }
@@ -38,9 +57,29 @@ fn get_vals(args: &ArgMatches) -> impl Iterator<Item = f64> {
         .filter_map(|x| x.unwrap().parse::<f64>().ok())
 }
 
+fn approx(x: f64) -> f64 {
+    let mut i: u64 = x.to_bits();
+    i = (MAGIC64 as f64 + (ONE_THIRD * i as f64)) as u64;
+    f64::from_bits(i)
+}
+
+fn fast_approx(x: f64) -> f64 {
+    let mut i: u64 = x.to_bits();
+    // approximately divide by 3, add to MAGIC; see
+    // http://www.hackersdelight.org/hdcodetxt/acbrt.c.txt
+    i = i / 4 + i / 16;
+    i += i / 16;
+    i += i / 256;
+    i += MAGIC64;
+    f64::from_bits(i)
+}
+
 fn get_rooter(args: &ArgMatches) -> Box<dyn Fn(f64) -> f64> {
     match args.value_of("variant").unwrap() {
-        _ => Box::new(move |x| x.cbrt()),
+        "exact" => Box::new(|x| x.cbrt()),
+        "approx" => Box::new(approx),
+        "fast-approx" => Box::new(fast_approx),
+        _ => panic!("why did you do this"),
     }
 }
 
